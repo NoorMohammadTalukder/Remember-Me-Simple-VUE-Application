2289726885b79816cc932cca53ea4674e19ed666
YourName <noor11809027@gmail.com>
2022-09-07 19:26:32 +0600

    resouce load

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-    <ul></ul>
+    
+  <ul>
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    >
+    </learning-resources>
+  </ul>
 </template>
 
 <script>
+import LearningResources from './components/learning-resources/LearningResource.vue';
 export default {
-    data(){
-        return{
-            storedResources:[
-                {
-                    id:"official-guide",
-                    title:"Official Guide",
-                    description:"The official Vue.js documentation",
-                    link:"https://vuejs.org",
-                },
-                {
-                    id:"google",
-                    title:"Google",
-                    description:"Learn Google",
-                    link:"https://google.org",
-                },
-            ]
-        }
-    }
-}
+  components: {
+   LearningResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn Google',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
 </script>
