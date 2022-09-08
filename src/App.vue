f63fe1f99484c76fd38ff2edc955c5830e52e31f
YourName <noor11809027@gmail.com>
2022-09-08 12:44:24 +0600

    learning resource, stored  resouces added

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,46 @@
 <template>
-    
-  <ul>
-    <learning-resources
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    >
-    </learning-resources>
-  </ul>
+ <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResources from './components/learning-resources/LearningResource.vue';
-export default {
-  components: {
-   LearningResources,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn Google',
-          link: 'https://google.org',
-        },
-      ],
+    import StoredResources from './components/learning-resources/StoredResources.vue';
+    export default {
+      components: {
+        StoredResources,
+      },
+      data() {
+        return {
+          storedResources: [
+            {
+              id: 'official-guide',
+              title: 'Official Guide',
+              description: 'The official Vue.js documentation',
+              link: 'https://vuejs.org',
+            },
+            {
+              id: 'google',
+              title: 'Google',
+              description: 'Learn Google',
+              link: 'https://google.org',
+            },
+          ],
+        };
+      },
     };
-  },
-};
 </script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+      box-sizing: border-box;
+    }
+
+    html {
+      font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+      margin: 0;
+    }
+</style>
